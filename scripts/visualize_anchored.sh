07950b8e8116e71950992b4b080e7b94a37d8aef
yuplin2333 <linyupin@msu.edu>
2024-07-26 10:58:47 -0400

    Fix typo

--- scripts/visualize_anchored.sh
+++ scripts/visualize_anchored.sh
@@ -1,7 +1,7 @@
 python visualizer_anchored.py \
     --model "meta-llama/Llama-2-7b-chat-hf" \
     --anchors "./data/prompt-driven_benign.txt" "./data/prompt-driven_harmful.txt" \
-    --datasets "./data/visualization/gcg_llama2-7b_original.csv" "./data/visualization/gcg_llama2-7b_jailbreak.csv" "./data/avisualization/gcg_llama2-7b_jailbreak_failed.csv" \
+    --datasets "./data/visualization/gcg_llama2-7b_original.csv" "./data/visualization/gcg_llama2-7b_jailbreak.csv" "./data/visualization/gcg_llama2-7b_jailbreak_failed.csv" \
     --text_columns "source" "source" "source" \
     --colors "bkgry" \
     --labels "Anchor Harmless" "Anchor Harmful" "Jailbreak Original" "Jailbreak Succeeded" "Jailbreak Failed" \
