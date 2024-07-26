6c55e931da9947e46f2ee5226fc8ae10d2b6cfea
yuplin2333 <linyupin@msu.edu>
2024-07-26 10:57:17 -0400

    Correct example of usage

--- scripts/visualize_anchored.sh
+++ scripts/visualize_anchored.sh
@@ -1,8 +1,9 @@
 python visualizer_anchored.py \
     --model "meta-llama/Llama-2-7b-chat-hf" \
     --anchors "./data/prompt-driven_benign.txt" "./data/prompt-driven_harmful.txt" \
-    --datasets "./data/autodan_llama2_original.csv" "./data/autodan_llama2_jailbreak.csv" "./data/autodan_llama2_jailbreak_failed.csv" \
+    --datasets "./data/visualization/gcg_llama2-7b_original.csv" "./data/visualization/gcg_llama2-7b_jailbreak.csv" "./data/avisualization/gcg_llama2-7b_jailbreak_failed.csv" \
+    --text_columns "source" "source" "source" \
     --colors "bkgry" \
     --labels "Anchor Harmless" "Anchor Harmful" "Jailbreak Original" "Jailbreak Succeeded" "Jailbreak Failed" \
     --num_components 2 \
-    --output ./visualization/pca_autodan_llama2-7b.png
+    --output ./visualization/pca_gcg_llama2-7b.png
